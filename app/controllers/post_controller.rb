9752b75a23b502cab207ac16b03b50427abb7a57
PineGolden311 <software.developer112@gmail.com>
2019-03-07 17:53:43 +0400

    add cloudinary config

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -3,5 +3,13 @@ class PostController < ApplicationController
   end
 
   def store
+    # upload image to cloudinary
+    @image = Cloudinary::Uploader.upload(params[:media])
+    # create a new post object and save to db
+    @post = Post.new({:title => params[:title], :body => params[:body], :author => params[:author], :media => @image['secure_url']})
+    @post.save
+    #triger an event with pusher
+
+    redirect_to('/')
   end
 end
